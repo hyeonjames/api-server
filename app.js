9b070ac515136ffd0274db6e9c4f787347c42247
tmdgus0118 <qoohk@u-crm.com>
2016-05-17 16:07:47 +0900

    cors

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ const app = express();
 const redis = require('redis');
 const redisStore = require('connect-redis')(session);
 const redisClient = redis.createClient();
+const cors = require('cors');
 
 mong.connect('mongodb://localhost/codeweb', function (err) {
     app.listen(3021);
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
+app.use(cors({
+    origin : /code404\.co\.kr$/
+}));
 app.use(session({
     secret: 'codeweb',
     resave: false,
